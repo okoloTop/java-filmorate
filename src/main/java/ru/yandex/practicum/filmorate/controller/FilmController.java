8df9ae5b7b791e3a3db2865cf89e516fb8c457e9
okoloTop <medvedkov82@yandex.ru>
2023-03-30 17:54:55 +0700

    ТЗ 11 на проверку

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -44,7 +44,7 @@ public class FilmController {
 
     @GetMapping("/films/popular")
     public List<Film> findAll(@RequestParam(value = "count", defaultValue = "10", required = false) Integer count) {
-        if(count <= 0){
+        if (count <= 0) {
             throw new IllegalArgumentException();
         }
         return filmService.findAllPopular(count);
@@ -53,19 +53,21 @@ public class FilmController {
     @PutMapping("/films/{id}/like/{userId}")
     public void likeFilm(@PathVariable("id") Integer filmId, @PathVariable("userId") Integer userId) {
         log.debug("Получен запрос PUT /films/{id}/like/{userId}.");
-        filmService.likeFilm(filmId,userId);
+        filmService.likeFilm(filmId, userId);
     }
 
     @DeleteMapping("/films/{id}/like/{userId}")
     public void deleteLikeFilm(@PathVariable("id") Integer filmId, @PathVariable("userId") Integer userId) {
         log.debug("Получен запрос DELETE /films/{id}/like/{userId}.");
-        filmService.deleteLikeFilm(filmId,userId);
+        filmService.deleteLikeFilm(filmId, userId);
     }
+
     @GetMapping("/genres")
     public List<Genre> getGenres() {
         log.debug("Получен запрос GET /genres.");
         return filmService.getAllGenres();
     }
+
     @GetMapping("/genres/{id}")
     public Genre getGenre(@PathVariable("id") Integer genreId) {
         log.debug("Получен запрос GET /genres/{id}.");
@@ -77,6 +79,7 @@ public class FilmController {
         log.debug("Получен запрос GET /mpa.");
         return filmService.getAllMpa();
     }
+
     @GetMapping("/mpa/{id}")
     public MPA getMPA(@PathVariable("id") Integer genreId) {
         log.debug("Получен запрос GET /mpa/{id}.");
